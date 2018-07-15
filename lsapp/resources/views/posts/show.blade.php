1115c11a98f29bdc6c80b985873695d395af7b00
Alaa Alakkad <alaaalakkad@gmail.com>
2018-07-16 00:54:05 +0200

    posts views added

---
+++ lsapp/resources/views/posts/show.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+@section('content')
+    <h1>{{$post-> title}}</h1>
+    <div>
+        {{$post->body}}
+    </div>
+    <small>Written on {{$post->created_at}}</small>
+    <div>
+    <a href="/posts" class="btn btn-outline-secondary btn-sm">Go Back</a>
+    </div>
+    @endsection('content')
